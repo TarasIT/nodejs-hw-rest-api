4ccdc2b6b6f98e274d29fc429eaf22a7fde8ea41
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-12-23 20:47:22 +0200

    server is cleaned from comments

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
 const app = require("./app");
-// const {
-//   listContacts,
-//   getById,
-//     addContact,
-//     removeContact,
-//     updateContact,
-// } = require("./handlers/contactsHandlers");
-
-// async function name(req, res) {
-//   console.log("contacts list =>", await listContacts());
-// }
-// name();
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
